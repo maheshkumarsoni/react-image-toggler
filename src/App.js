cdfbee48630ad5c29bfd4f7ad6fb1b1a2819cef9
maheshkumarsoni <sms3299sms@gmail.com>
2020-09-19 21:28:31 +0530

    just write starter code for our app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1> Welcome to Class Based Component.</h1 >
+        );
+    }
+}
+
+export default App;
